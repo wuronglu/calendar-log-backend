ade3a6220a93a3b271c59c46e5b1a052714d5d9a
wuronglu <2923239110@qq.com>
2024-11-26 11:11:49 +0800

    modify cors

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"calendar-log-backend/config"
+	"calendar-log-backend/middlewares"
 	"calendar-log-backend/routes"
 
 	"github.com/gin-gonic/gin"
@@ -10,6 +11,7 @@ import (
 func main() {
 	r := gin.Default()
 
+	r.Use(middlewares.CORSMiddleware())
 	routes.RegisterRoutes(r)
 
 	r.Run(config.SERVER_PORT)
---
+++ middlewares/cors.go
@@ -0,0 +1,25 @@
+package middlewares
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+// CORS 中间件
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Authorization, Accept, Origin, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS")
+
+		// 处理预检请求
+		if c.Request.Method == http.MethodOptions {
+			c.AbortWithStatus(http.StatusNoContent)
+			return
+		}
+
+		c.Next()
+	}
+}
