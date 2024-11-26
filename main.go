f48f57f8b7d9e59a4242637958c5d74a8fb5bfd1
wuronglu <2923239110@qq.com>
2024-11-26 12:59:06 +0800

    modify port,use .env file

--- main.go
+++ main.go
@@ -1,18 +1,31 @@
 package main
 
 import (
-	"calendar-log-backend/config"
 	"calendar-log-backend/middlewares"
 	"calendar-log-backend/routes"
+	"log"
+	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	// 加载 .env 文件
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
 	r := gin.Default()
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "9191"
+	}
 
 	r.Use(middlewares.CORSMiddleware())
 	routes.RegisterRoutes(r)
 
-	r.Run(config.SERVER_PORT)
+	r.Run(":" + port)
 }
